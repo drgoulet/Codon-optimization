717859116c0f9e3254917506d30874ab71de5467
drgoulet <67811707+drgoulet@users.noreply.github.com>
2022-04-24 12:42:48 -0700

    Update codon_counter.py

--- codon_counter.py
+++ codon_counter.py
@@ -1,28 +1,33 @@
-with open('seq-opt.txt') as f:
-    seq_idt = f.read().upper()
+import os
 
-with open('seq-orig.txt') as f:
-    seq_opt = f.read().upper()
+os.chdir('/mnt/c/Users/dgoulet/Scripts/codon-optimization/')
+
+with open('pdl1-gm.txt') as f:
+    seq_1 = f.read().upper()
+
+with open('pdl1-rnn.txt') as f:
+    seq_2 = f.read().upper()
 
 def encrypt(string,length):
     return ' '.join(string[i:i+length] for i in range(0,len(string),length))
 
-codons_opt_pre = encrypt(seq_opt,3)
+codons_1_pre = encrypt(seq_1,3)
+codons_2_pre = encrypt(seq_2,3)
 
-codons_idt = seq_idt.split()
-codons_opt = codons_opt_pre.split()
+codons_1 = codons_1_pre.split()
+codons_2 = codons_2_pre.split()
 
-seq_idt_nospace = seq_idt.replace(" ", "")
-seq_opt_nospace = seq_opt.replace(" ", "")
+seq_1_nospace = seq_1.replace(" ", "")
+seq_2_nospace = seq_2.replace(" ", "")
 
-bases_idt = [char for char in seq_idt_nospace]
-bases_opt = [char for char in seq_opt_nospace]
+bases_1 = [char for char in seq_1_nospace]
+bases_2 = [char for char in seq_2_nospace]
 
 yes = 0
 no = 0
 
-for i in range(len(codons_idt)):
-    if codons_idt[i] == codons_opt[i]:
+for i in range(len(codons_1)):
+    if codons_1[i] == codons_2[i]:
         yes += 1
     else:
         no += 1
@@ -30,8 +35,8 @@ for i in range(len(codons_idt)):
 hai = 0
 iie = 0
 
-for i in range(len(bases_idt)):
-    if bases_idt[i] == bases_opt[i]:
+for j in range(len(bases_1)):
+    if bases_1[j] == bases_2[j]:
         hai += 1
     else:
         iie += 1
@@ -45,57 +50,56 @@ percent_match = yes / total * 100
 print(f'The DNA sequences are {round(percent_overall,1)}% identical. ({hai}/{total_overall} bases)')
 print(f'The codon usage is {round(percent_match,1)}% identical. ({yes}/{total} codons)')
 
-idt_a = 0
-idt_c = 0
-idt_g = 0
-idt_t = 0
+seq1_a = 0
+seq1_c = 0
+seq1_g = 0
+seq1_t = 0
 
-for base in bases_idt:
+for base in bases_1:
     if base == 'A':
-        idt_a += 1
+        seq1_a += 1
     elif base == 'C':
-        idt_c += 1
+        seq1_c += 1
     elif base == 'G':
-        idt_g += 1
+        seq1_g += 1
     elif base == 'T':
-        idt_t += 1
+        seq1_t += 1
 
-idt_total = idt_a + idt_c + idt_g + idt_t
-idt_pct_a = round(idt_a / idt_total * 100, 1)
-idt_pct_c = round(idt_c / idt_total * 100, 1)
-idt_pct_g = round(idt_g / idt_total * 100, 1)
-idt_pct_t = round(idt_t / idt_total * 100, 1)
+seq1_total = seq1_a + seq1_c + seq1_g + seq1_t
+seq1_pct_a = round(seq1_a / seq1_total * 100, 1)
+seq1_pct_c = round(seq1_c / seq1_total * 100, 1)
+seq1_pct_g = round(seq1_g / seq1_total * 100, 1)
+seq1_pct_t = round(seq1_t / seq1_total * 100, 1)
 
-print('IDT:')
-print(f'A: {idt_pct_a}')
-print(f'C: {idt_pct_c}')
-print(f'G: {idt_pct_g}')
-print(f'T: {idt_pct_t}')
+print('Seq1:')
+print(f'A: {seq1_pct_a}')
+print(f'C: {seq1_pct_c}')
+print(f'G: {seq1_pct_g}')
+print(f'T: {seq1_pct_t}')
 
-opt_a = 0
-opt_c = 0
-opt_g = 0
-opt_t = 0
+seq2_a = 0
+seq2_c = 0
+seq2_g = 0
+seq2_t = 0
 
-for base in bases_opt:
+for base in bases_2:
     if base == 'A':
-        opt_a += 1
+        seq2_a += 1
     elif base == 'C':
-        opt_c += 1
+        seq2_c += 1
     elif base == 'G':
-        opt_g += 1
+        seq2_g += 1
     elif base == 'T':
-        opt_t += 1
+        seq2_t += 1
 
-opt_total = opt_a + opt_c + opt_g + opt_t
-opt_pct_a = round(opt_a / opt_total * 100, 1)
-opt_pct_c = round(opt_c / opt_total * 100, 1)
-opt_pct_g = round(opt_g / opt_total * 100, 1)
-opt_pct_t = round(opt_t / opt_total * 100, 1)
+seq2_total = seq2_a + seq2_c + seq2_g + seq2_t
+seq2_pct_a = round(seq2_a / seq2_total * 100, 1)
+seq2_pct_c = round(seq2_c / seq2_total * 100, 1)
+seq2_pct_g = round(seq2_g / seq2_total * 100, 1)
+seq2_pct_t = round(seq2_t / seq2_total * 100, 1)
 
-print('Opt:')
-print(f'A: {opt_pct_a}')
-print(f'C: {opt_pct_c}')
-print(f'G: {opt_pct_g}')
-print(f'T: {opt_pct_t}')
-
+print('Seq2:')
+print(f'A: {seq2_pct_a}')
+print(f'C: {seq2_pct_c}')
+print(f'G: {seq2_pct_g}')
+print(f'T: {seq2_pct_t}')
